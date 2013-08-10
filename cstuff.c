35f71454bc21840d86ccf6c4595313c102b294ec
Andrew <andrewjamescollett@gmial.com>
2013-08-10 14:21:50 +0200

    Update cstuff.c

--- cstuff.c
+++ cstuff.c
@@ -3,7 +3,18 @@
 
 int main (int argc , char* argv)
 {
-
+	
+	char s[] = {"this is so cool"};
+	int cnt = 0;
+	
+	
 	printf("Hello Wolrds! Git is cool/n");
+	
+	for (cnt = 0: cnt < s.size() : cnt++)
+	{
+		printf("%c", s[cnt]);
+	}
+	
+	printf("/n");
 
 }
