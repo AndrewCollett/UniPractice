ad1a9195338c46d98b6a102bb13ecebd4b579959
Andrew James Collett <andrewjamescollett@gmail.com>
2013-08-10 14:28:08 +0200

    this is fixed

--- cstuff.c
+++ cstuff.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc , char* argv)
+int main (int argc , char* argv[])
 {
 	
 	char s[] = {"this is so cool"};
 	int cnt = 0;
 	
 	
-	printf("Hello Wolrds! Git is cool/n");
+	printf("Hello Wolrds! Git is cool\n");
 	
-	for (cnt = 0: cnt < s.size() : cnt++)
+	for (cnt = 0; cnt < sizeof(s); cnt++)
 	{
 		printf("%c", s[cnt]);
 	}
 	
-	printf("/n");
-
+	printf("\n");
+	
+	return EXIT_SUCCESS;
 }
